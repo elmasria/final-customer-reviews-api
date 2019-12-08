07d72bd4befddee771a0e5fb662567c5743f627e
Ahmad El Masri <ahmadjelmasri@gmail.com>
2019-12-08 14:02:30 +0100

    feat: update controllers

--- src/main/java/com/udacity/course3/reviews/controller/CommentsController.java
+++ src/main/java/com/udacity/course3/reviews/controller/CommentsController.java
@@ -1,11 +1,20 @@
 package com.udacity.course3.reviews.controller;
 
+import com.udacity.course3.reviews.entity.nosql.CommentDocument;
+import com.udacity.course3.reviews.entity.sql.Comment;
+import com.udacity.course3.reviews.entity.sql.Review;
+import com.udacity.course3.reviews.repository.CommentDocumentsRepository;
+import com.udacity.course3.reviews.repository.CommentsRepository;
+import com.udacity.course3.reviews.repository.ReviewDocumentsRepository;
+import com.udacity.course3.reviews.repository.ReviewsRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpServerErrorException;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring REST controller for working with comment entity.
@@ -14,34 +23,51 @@ import java.util.List;
 @RequestMapping("/comments")
 public class CommentsController {
 
-    // TODO: Wire needed JPA repositories here
+    private final ReviewsRepository reviewsRepository;
+    private final CommentsRepository commentsRepository;
+    private final ReviewDocumentsRepository reviewDocumentsRepository;
+    private final CommentDocumentsRepository commentDocumentsRepository;
 
-    /**
-     * Creates a comment for a review.
-     *
-     * 1. Add argument for comment entity. Use {@link RequestBody} annotation.
-     * 2. Check for existence of review.
-     * 3. If review not found, return NOT_FOUND.
-     * 4. If found, save comment.
-     *
-     * @param reviewId The id of the review.
-     */
-    @RequestMapping(value = "/reviews/{reviewId}", method = RequestMethod.POST)
-    public ResponseEntity<?> createCommentForReview(@PathVariable("reviewId") Integer reviewId) {
-        throw new HttpServerErrorException(HttpStatus.NOT_IMPLEMENTED);
+    public CommentsController(ReviewsRepository reviewsRepository, CommentsRepository commentsRepository, ReviewDocumentsRepository reviewDocumentsRepository, CommentDocumentsRepository commentDocumentsRepository) {
+        this.reviewsRepository = reviewsRepository;
+        this.commentsRepository = commentsRepository;
+        this.reviewDocumentsRepository = reviewDocumentsRepository;
+        this.commentDocumentsRepository = commentDocumentsRepository;
     }
 
     /**
-     * List comments for a review.
-     *
-     * 2. Check for existence of review.
-     * 3. If review not found, return NOT_FOUND.
-     * 4. If found, return list of comments.
-     *
+     * @param reviewId The id of the review.
+     */
+    @RequestMapping(value = "/reviews/{reviewId}", method = RequestMethod.POST)
+    public ResponseEntity<Comment> createCommentForReview(@PathVariable("reviewId") Integer reviewId, @RequestBody @Valid Comment comment) {
+        Optional<Review> optionalReview = reviewsRepository.findById(reviewId);
+        if (optionalReview.isPresent()) {
+            Review review = optionalReview.get();
+            comment.setReview(review);
+            comment = commentsRepository.save(comment);
+
+            CommentDocument commentDocument = new CommentDocument();
+            commentDocument.setId(comment.getId());
+            commentDocument.setComment(comment.getComment());
+            commentDocument.setOwner(comment.getOwnerName());
+            commentDocument.setReviewId(comment.getReview().getId());
+
+            commentDocumentsRepository.save(commentDocument);
+
+            return new ResponseEntity<>(comment, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /**
      * @param reviewId The id of the review.
      */
     @RequestMapping(value = "/reviews/{reviewId}", method = RequestMethod.GET)
-    public List<?> listCommentsForReview(@PathVariable("reviewId") Integer reviewId) {
-        throw new HttpServerErrorException(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<List<CommentDocument>> listCommentsForReview(@PathVariable("reviewId") Integer reviewId) {
+        List<CommentDocument> reviewsList = commentDocumentsRepository.findAllByReviewId(reviewId);
+        if (reviewsList != null) {
+            return new ResponseEntity<>(reviewsList, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/udacity/course3/reviews/controller/ReviewsController.java
+++ src/main/java/com/udacity/course3/reviews/controller/ReviewsController.java
@@ -1,11 +1,18 @@
 package com.udacity.course3.reviews.controller;
 
+import com.udacity.course3.reviews.entity.nosql.ReviewDocument;
+import com.udacity.course3.reviews.entity.sql.Product;
+import com.udacity.course3.reviews.entity.sql.Review;
+import com.udacity.course3.reviews.repository.ProductsRepository;
+import com.udacity.course3.reviews.repository.ReviewDocumentsRepository;
+import com.udacity.course3.reviews.repository.ReviewsRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpServerErrorException;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring REST controller for working with review entity.
@@ -13,32 +20,52 @@ import java.util.List;
 @RestController
 public class ReviewsController {
 
-    // TODO: Wire JPA repositories here
+    private final ReviewsRepository reviewsRepository;
+    private final ProductsRepository productsRepository;
+    private final ReviewDocumentsRepository reviewDocumentsRepository;
+
+    public ReviewsController(ReviewsRepository reviewsRepository, ProductsRepository productsRepository, ReviewDocumentsRepository reviewDocumentsRepository) {
+        this.reviewsRepository = reviewsRepository;
+        this.productsRepository = productsRepository;
+        this.reviewDocumentsRepository = reviewDocumentsRepository;
+    }
 
     /**
-     * Creates a review for a product.
-     * <p>
-     * 1. Add argument for review entity. Use {@link RequestBody} annotation.
-     * 2. Check for existence of product.
-     * 3. If product not found, return NOT_FOUND.
-     * 4. If found, save review.
-     *
      * @param productId The id of the product.
      * @return The created review or 404 if product id is not found.
      */
     @RequestMapping(value = "/reviews/products/{productId}", method = RequestMethod.POST)
-    public ResponseEntity<?> createReviewForProduct(@PathVariable("productId") Integer productId) {
-        throw new HttpServerErrorException(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<Review> createReviewForProduct(@PathVariable("productId") Integer productId, @RequestBody @Valid Review review) {
+        Optional<Product> optionalProduct = productsRepository.findById(productId);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            review.setProduct(product);
+            review = reviewsRepository.save(review);
+
+            ReviewDocument reviewDocument = new ReviewDocument();
+            reviewDocument.setOwner(review.getOwnerName());
+            reviewDocument.setId(review.getId());
+            reviewDocument.setProductId(review.getProduct().getId());
+            reviewDocument.setReview(review.getReview());
+            reviewDocument.setTitle(review.getTitle());
+
+            reviewDocumentsRepository.save(reviewDocument);
+
+            return new ResponseEntity<>(review, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     /**
-     * Lists reviews by product.
-     *
      * @param productId The id of the product.
      * @return The list of reviews.
      */
     @RequestMapping(value = "/reviews/products/{productId}", method = RequestMethod.GET)
-    public ResponseEntity<List<?>> listReviewsForProduct(@PathVariable("productId") Integer productId) {
-        throw new HttpServerErrorException(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<List<ReviewDocument>> listReviewsForProduct(@PathVariable("productId") Integer productId) {
+        List<ReviewDocument> reviews = reviewDocumentsRepository.findAllByProductId(productId);
+        if (reviews != null){
+            return new ResponseEntity<>(reviews, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
